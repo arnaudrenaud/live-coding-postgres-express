a96fedd6b3179d5f780086031e4cf72a7a62a116
Arnaud Renaud <arnaud.renaud@gmail.com>
2021-07-08 16:06:06 +0200

    Run migrations with connection config from environment

--- data-access/migrate.js
+++ data-access/migrate.js
@@ -1,12 +1,17 @@
+const dotenv = require("dotenv");
 const { migrate } = require("postgres-migrations");
 
+dotenv.config();
+
 const runMigrations = async () => {
+  const { PGUSER, PGHOST, PGPASSWORD, PGDATABASE, PGPORT } = process.env;
+
   const dbConfig = {
-    database: "postgres",
-    user: "postgres",
-    password: "mysecretpassword",
-    host: "localhost",
-    port: 5432,
+    database: PGDATABASE,
+    user: PGUSER,
+    password: PGPASSWORD,
+    host: PGHOST,
+    port: +PGPORT,
   };
 
   await migrate(dbConfig, "data-access/database-migrations/");
