f963b7e96404a57f6ab6e760b5454f5c31e5f0d7
Arnaud Renaud <arnaud.renaud@gmail.com>
2021-07-08 16:23:34 +0200

    Do not return id in `getRestaurants`, add function `createRestaurant`

--- data-access/index.js
+++ data-access/index.js
@@ -6,9 +6,21 @@ dotenv.config();
 const pool = new Pool();
 
 const getRestaurants = async () => {
-  return pool.query("SELECT * FROM restaurant;");
+  const { rowCount, rows } = await pool.query(
+    "SELECT uuid, name, owner_id FROM restaurant;"
+  );
+  return { rowCount, rows };
+};
+
+const createRestaurant = async (name, ownerId) => {
+  const { rowCount, rows } = await pool.query(
+    "INSERT INTO restaurant(name, owner_id) VALUES ($1, $2) RETURNING uuid, name, owner_id;",
+    [name, ownerId]
+  );
+  return { rowCount, rows };
 };
 
 module.exports = {
   getRestaurants,
+  createRestaurant,
 };
