3ec49f0adafde45cd5ff8eb065de3e26256b4617
Arnaud Renaud <arnaud.renaud@gmail.com>
2021-07-09 16:21:28 +0200

    Use `asyncErrorHandler` to factorise error handling

--- http/routers/restaurants.js
+++ http/routers/restaurants.js
@@ -1,45 +1,48 @@
 const express = require("express");
+
 const {
   getRestaurants,
   getRestaurantsByOwnerId,
   getRestaurantByUuid,
   createRestaurant,
 } = require("../../data-access");
+const { asyncErrorHandler } = require("../utils");
 
 const router = express.Router();
 
 const getAuthenticatedUserId = () => 1;
 
-router.get("/", async (req, res) => {
-  if (req.query.filter === "mine") {
-    const authenticatedUserId = getAuthenticatedUserId();
-    res.json({
-      restaurants: (await getRestaurantsByOwnerId(authenticatedUserId)).rows,
-    });
-  } else {
-    res.json({ restaurants: (await getRestaurants()).rows });
-  }
-});
+router.get(
+  "/",
+  asyncErrorHandler(async (req, res) => {
+    if (req.query.filter === "mine") {
+      const authenticatedUserId = getAuthenticatedUserId();
+      res.json({
+        restaurants: (await getRestaurantsByOwnerId(authenticatedUserId)).rows,
+      });
+    } else {
+      res.json({ restaurants: (await getRestaurants()).rows });
+    }
+  })
+);
 
-router.get("/:uuid", async (req, res, next) => {
-  const { uuid } = req.params;
-  try {
+router.get(
+  "/:uuid",
+  asyncErrorHandler(async (req, res) => {
+    const { uuid } = req.params;
     res.json(await getRestaurantByUuid(uuid));
-  } catch (error) {
-    next(error);
-  }
-});
+  })
+);
 
-router.post("/", async (req, res, next) => {
-  try {
+router.post(
+  "/",
+  asyncErrorHandler(async (req, res, next) => {
     const authenticatedUserId = getAuthenticatedUserId();
 
     const { name } = req.body;
     const createdRestaurant = await createRestaurant(name, authenticatedUserId);
     res.status(201).json(createdRestaurant);
-  } catch (error) {
-    next(error);
-  }
-});
+  })
+);
 
 module.exports = router;
---
+++ http/utils.js
@@ -0,0 +1,7 @@
+const asyncErrorHandler = (fn) => (req, res, next) => {
+  return Promise.resolve(fn(req, res, next)).catch(next);
+};
+
+module.exports = {
+  asyncErrorHandler,
+};
